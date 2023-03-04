7cd31b842dffab4e6a626e3f0a5e1b5a6d42889d
Armando Soares Sousa <armando.sousa@gmail.com>
2023-03-03 22:33:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from myapp import app
+from myapp import db, data_base
+import datetime
 
-app = Flask(__name__)
+CREATE_DB_EMPTY = False
 
+print('Iniciando a aplicação SysFile')
+# Dropa e recria o banco
+try:
+    if CREATE_DB_EMPTY:
+        db.drop_all()
+        db.create_all()
+        db.session.commit()
+        print(f'Data base {data_base} created with success!!')
+    else:
+        print(f'Data base {data_base} load successfully!')
+        print(f'Hora: {datetime.datetime.now()}')
+except Exception as e:
+    print(f'Error creating {data_base} - {e}')
 
-@app.route('/')
-def index():
-    return 'Teste Flask no Replit!'
-
-
-app.run(host='0.0.0.0', port=81)
+if __name__ == '__main__':
+    app.run(debug=True)
